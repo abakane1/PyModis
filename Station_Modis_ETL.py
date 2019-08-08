95b97674070e6bc94e1027f743fac1ee36615bbf
Zuliang Zhao <mrzzl1987@gmail.com>
2019-08-08 17:59:55 +0800

    add a new para

--- Station_Modis_ETL.py
+++ Station_Modis_ETL.py
@@ -13,13 +13,13 @@ except:
 
 
 # 因为Modis数据是按照一年的天数去存，而气象站点是按照日期去存
-def get_station_value_by_num_day(year, daydir, StationID, T_tpye='HighestTemperature'):
+def get_station_value_by_num_day(root_path, year, daydir, StationID, T_tpye='HighestTemperature'):
     now_time = datetime.datetime(int(year), int('01'), int('01'))
     f = now_time + datetime.timedelta(days=int(daydir) - 1)
     fu = f.strftime('%Y%m%d')
     month = f.month
     day = f.day
-    data = pd.read_csv('G:\\mosicData\\stationdata.csv')
+    data = pd.read_csv(root_path + '/meteodata.csv')
     station_value = data[(data['Year'] == int(year)) & (data['Months'] == int(month)) & (data['Days'] == int(day)) & (
             data['StationID'] == StationID)].head()
     try:
@@ -33,19 +33,23 @@ def get_station_value_by_num_day(year, daydir, StationID, T_tpye='HighestTempera
 
 # band # 1是白天，2是晚上
 # 读取一幅影像，找到上面所有的点
-def get_grid_value_by_station_value(filename, year, day, band):
-
-    lonlatlist = pd.read_csv('G:\\mosicData\\HSstation.csv')
+def get_grid_value_by_station_value(root_path, filename, year, day, band):
+    print(root_path)
+    lonlatlist = pd.read_csv( root_path + '/HHHstations.csv')
     for index, lonlatdata in lonlatlist.iterrows():
-        lon = float(lonlatdata['Longitude'])
-        lat = float(lonlatdata['Latitude'])
-        StationID = lonlatdata['StationID']
-        value = pfg.get_value_by_coordinates(filename, [lon, lat], band)
-        tem_value = 0.02 * value - 273.15
-        date, station_high_value = get_station_value_by_num_day(year, day, StationID)
-        row = str(StationID) + ',' + str(year) + ',' + str(date) + ',', str(tem_value), ',', str(station_high_value)
-        Modis_IO.write_txt("grid_station_day.txt", row)
-        print(row)
+        try:
+            lon = float(lonlatdata['Longitude'])
+            lat = float(lonlatdata['Latitude'])
+            StationID = lonlatdata['StationID']
+            value = pfg.get_value_by_coordinates(filename, [lon, lat], band)
+            tem_value = format(0.02 * value - 273.15, '.2f')
+            date, station_high_value = get_station_value_by_num_day(root_path, year, day, StationID)
+            row = str(StationID) + ',' + str(year) + ',' + str(date) + ',', str(tem_value), ',', str(station_high_value)
+            Modis_IO.write_txt("grid_station_day.txt", row)
+            print(row)
+        except:
+            print("No data")
+            continue
 
 
 def funcTest():
@@ -53,4 +57,4 @@ def funcTest():
     filename = 'G:\\mosicdata\\mosic2006\\168\\day\\result.tif'
     get_grid_value_by_station_value(filename,2006,168,1)
 
-funcTest()
+# funcTest()
