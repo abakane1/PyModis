a91a44f022f5588a7e50d8fa1d0ac9e33fbaa704
Zuliang Zhao <mrzzl1987@gmail.com>
2019-08-08 17:57:44 +0800

    add linux system (server)

--- Common_func.py
+++ Common_func.py
@@ -2,7 +2,7 @@ import platform
 
 def UsePlatform():
     """
-    工作平台包括windows和mac
+    工作平台包括windows,linux和mac
     读取数据要先判断
     :return: 数据root_path
     """
@@ -13,8 +13,8 @@ def UsePlatform():
     if(sysstr =='MacOS'):
         root_path = '/volumes/data/mosicData/'
     if (sysstr == 'Linux'):
-        root_path = '/home/seedserver/mosicData/'
-    #print (root_path)
+        root_path = '/home/seedserver/mosicdata/'
+    #print(root_path)
     return root_path
 
 UsePlatform()
