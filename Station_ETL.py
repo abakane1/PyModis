21381c758071ffc928cc5a01de5744b6827748db
Zuliang Zhao <mrzzl1987@gmail.com>
2019-08-15 11:28:35 +0800

    refactor

--- Station_ETL.py
+++ Station_ETL.py
@@ -5,25 +5,35 @@ import matplotlib.pyplot as plt
 import Common_func
 
 root_path = Common_func.UsePlatform()
-station_list = pd.read_csv(os.path.join(root_path, 'HHHstations.csv'))
-#print(os.path.join(root_path,'HHHstations.csv'))
 
-data = pd.read_csv(os.path.join(root_path,'meteodata.csv'))
 
-x = []
-y = []
-for index,station_data in station_list.iterrows():
-    station_ID = station_data['StationID']
-    station_name = station_data['StationName']
-    year_list = data[data['StationID'] == station_ID]['Year'].unique()
-    # print(station_ID,year_list)
-    print(station_ID,station_name,station_data['Latitude'],station_data['Longitude'],len(year_list))
-    x.append(station_ID)
-    y.append(len(year_list))
-    #for year in year_list:
+def station_year():
+    '''
+    查看气象站点有数据的年份
+    :return:
+    '''
+    station_list = pd.read_csv(os.path.join(root_path, 'HHHstations.csv'))
+    # print(os.path.join(root_path,'HHHstations.csv'))
+
+    data = pd.read_csv(os.path.join(root_path, 'meteodata.csv'))
+
+    x = []
+    y = []
+    for index, station_data in station_list.iterrows():
+        station_ID = station_data['StationID']
+        station_name = station_data['StationName']
+        year_list = data[data['StationID'] == station_ID]['Year'].unique()
+        # print(station_ID,year_list)
+        print(station_ID, station_name, station_data['Latitude'], station_data['Longitude'], len(year_list))
+        x.append(station_ID)
+        y.append(len(year_list))
+    # for year in year_list:
     #    x.append(station_name)
     #    y.append(year)
-# 年际分布
-#print(x,y)
-#plt.scatter(x,y)
-#plt.show()
+    # 年际分布
+    # print(x,y)
+    # plt.scatter(x,y)
+    # plt.show()
+
+def aro_station_year():
+    pass
